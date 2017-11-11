0e28062fe9ad5693cd224d08364923742495c652
Jan Mewes <jan.mewes@experimental-software.com>
2017-11-11 21:52:37 +0100

    Add first example unit test
    Example copied from http://www.throwtheswitch.org/unity , section "Getting started"

---
+++ src/DumbExample.c
@@ -0,0 +1,14 @@
+/*
+ * DumbExample.c
+ *
+ *  Created on: 11.11.2017
+ *      Author: janux
+ */
+#include "DumbExample.h"
+
+int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c)
+{
+return (int8_t)(((int16_t)a + (int16_t)b + (int16_t)c) / 3);
+}
+
+
