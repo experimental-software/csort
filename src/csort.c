1593c710684ef6c1424e4959ad9a797368fc0975
Jan Mewes <jan.mewes@epages.com>
2017-11-14 22:31:25 +0100

    Add draft for sorting implementation

--- src/csort.c
+++ src/csort.c
@@ -11,7 +11,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+	//SortingController_RegisterAlgorithm("quicksort", Quicksort)
+	//SortingController_RegisterAlgorithm("merge_sort", MergeSort)
+	
+	//print_array(SortingController_Sort(algorithm, numbers))
+	
 	argp_parse (0, argc, argv, 0, 0, 0);
 	puts("Hello, C World"); /* prints Hello, C World */
 	return EXIT_SUCCESS;
