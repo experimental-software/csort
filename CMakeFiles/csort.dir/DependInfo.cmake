ee898741e0f8edbab0adcd0258bd0673819b44ae
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-08-15 20:30:54 +0200

    WIP

---
+++ CMakeFiles/csort.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/janux/src/experimental-software/csort/src/Sorting.c" "CMakeFiles/csort.dir/src/Sorting.c.o" "gcc" "CMakeFiles/csort.dir/src/Sorting.c.o.d"
+  "/home/janux/src/experimental-software/csort/src/Utils.c" "CMakeFiles/csort.dir/src/Utils.c.o" "gcc" "CMakeFiles/csort.dir/src/Utils.c.o.d"
+  "/home/janux/src/experimental-software/csort/src/csort.c" "CMakeFiles/csort.dir/src/csort.c.o" "gcc" "CMakeFiles/csort.dir/src/csort.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/csort.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csort.dir/src/Sorting.c.o"
+  "CMakeFiles/csort.dir/src/Sorting.c.o.d"
+  "CMakeFiles/csort.dir/src/Utils.c.o"
+  "CMakeFiles/csort.dir/src/Utils.c.o.d"
+  "CMakeFiles/csort.dir/src/csort.c.o"
+  "CMakeFiles/csort.dir/src/csort.c.o.d"
+  "csort"
+  "csort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/csort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
